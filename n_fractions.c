684e413805af7edf79e4a17c38a9368fc05cb5c7
nandan-pai <53964874+nandan-pai@users.noreply.github.com>
2021-02-07 21:25:06 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,51 +1,76 @@
 //WAP to find the sum of n fractions.
-#include<stdio.h>
+#include <stdio.h>
 
 struct fraction
 {
-    float num;
-    float den;
+    int num;
+    int den;
 };
-typedef struct fraction FRACTION;
 
-FRACTION input(int j)
-{
-    FRACTION f;
-    printf("Enter Numerator %d : ",j);
-    scanf("%f",&f.num);
-    printf("Enter Denominator %d : ",j);
-    scanf("%f",&f.den);
-    return f;
-}
+typedef struct fraction f;
 
-int input_limit(int n)
+int input_limit()
 {
+    int n;
     printf("Enter number of fractions to be added: ");
     scanf("%d",&n);
     return n;
 }
 
-float fract(FRACTION f)
+void arrayinput(int n,f *frac)
 {
-    return f.num/f.den;
+    for(int i = 0; i<n; i++)
+    {
+        printf("Enter values for fraction %d\n",i+1);
+        printf("Numerator %d: ",i+1);
+        scanf("%d",&frac[i].num);
+        printf("Denominator %d: ",i+1);
+        scanf("%d",&frac[i].den);
+    }
 }
 
-int output(float total)
+int gcd(int a, int b) 
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b); 
+} 
+  
+int lcm(int n,f *frac) 
+{ 
+    int ans = frac[0].den; 
+    for (int i = 1; i < n; i++) 
+        ans = (((frac[i].den * ans)) / (gcd(frac[i].den, ans))); 
+    return ans; 
+} 
+
+int finalnum(int n,int deno,f * frac)
 {
-    printf("Sum of the fractions is: %f",total);
+    int numsum = 0;
+    for(int i=0;i<n;i++)
+    {
+       frac[i].num = frac[i].num * (deno/frac[i].den); 
+    }
+    for(int i=0;i<n;i++)
+    {
+        numsum += frac[i].num;
+    }
+    return numsum;
 }
 
-int main(void)
+void output(int nume,int deno)
+{
+    printf("Sum of given fractions is: %d/%d",nume,deno);
+}
+
+int main()
 {
     int n;
-    n = input_limit(n);
-    float sum = 0;
-    FRACTION f;
-    for(int i=0; i<n ;i++)
-    {
-        f = input(i+1);
-        sum += fract(f);
-    }
-    output(sum);
+    n = input_limit();
+    struct fraction frac[n];
+    arrayinput(n,frac);
+    int deno = lcm(n,frac);
+    int nume = finalnum(n,deno,frac);
+    output(nume,deno);
     return 0;
 }
