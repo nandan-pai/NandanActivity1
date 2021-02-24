3251fecafde58aae51c45f4f6c54b0e86a391a59
nandan-pai <53964874+nandan-pai@users.noreply.github.com>
2021-02-24 11:00:42 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,43 +1,60 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
-#include<conio.h>
-typedef struct
-{
-    int num;
-    int den;
-}FRACTION;
 
-FRACTION input1(int *num1,int *den1)
+struct fraction
+{
+    int num,den;
+};
+
+typedef struct fraction frac;
+
+frac input(int *num1,int *den1,int *num2, int *den2)
 {
     printf("For fraction 1 enter numerator and denominator: ");
     scanf("%d%d",num1,den1);
-}
-
-FRACTION input2(int *num2, int *den2)
-{
     printf("For fraction 2 enter numerator and denominator: ");
     scanf("%d%d",num2,den2);
 }
- 
-FRACTION sum(FRACTION f1, FRACTION f2)
+
+frac simplify(frac sum,int gcd)
 {
-    FRACTION result={(f1.num * f2.den) + (f2.num * f1.den), f1.den * f2.den};
+    sum.num = sum.num / gcd;
+    sum.den = sum.den / gcd;
+    return sum;
+}
+ 
+int findgcd(int num,int den)
+{
+    int gcd;
+    for(int i = 2 ; i <= num && i <= den; i++)
+        if(num % i == 0 && den % i == 0)
+            gcd = i;
+    return gcd;
+}
+
+frac add(frac f1,frac f2)
+{
+    frac result;
+    result.den = f1.num * f2.den;
+    result.num = (f1.num * f2.den) + (f2.num * f1.den);
+    
     return result;
 }
 
-FRACTION output(FRACTION f1, FRACTION f2)
+frac output(frac sum)
 {
-    FRACTION total = sum(f1,f2);
-    printf("Result=%d/%d",total.num,total.den);
+    printf("Result = %d/%d",sum.num,sum.den);
 }
 
-int main()
+int main(void)
 {
-    int num1,den1,num2,den2;
-    input1(&num1,&den1);
-    input2(&num2,&den2);
-    FRACTION f1={num1, den1};
-    FRACTION f2 ={num2, den2};
-    output(f1,f2);
+    int num1,den1,num2,den2,gcd;
+    input(&num1,&den1,&num2,&den2);
+    frac f1 = {num1, den1};
+    frac f2 = {num2, den2};
+    frac sum = add(f1,f2);
+    gcd = findgcd(sum.num,sum.den);
+    sum = simplify(sum,gcd);
+    output(sum);
     return 0;
 }
